8fd09a4ffa3dbff519f416775e337054015bb6a0
Bailey Garrett <baileyagarrett@gmail.com>
2020-04-03 23:14:08 -0500

    working on getting urls for auth0 to work

--- src/App.js
+++ src/App.js
@@ -1,45 +1,65 @@
 import React, { Component } from "react";
 //Import Home in order to render Home Component
 // import Home from'./components/container/Home/Home';
-import Navbar from "./components/container/Navbar/Navbar";
+import NavBar from "./components/container/Navbar/Navbar";
 //Configure the routes by importing the default export from the routes.js file,
 //Then delete the Home component, and put the routes underneath the nav.
 import routes from "./routes";
 //import withROuter since you are using redux, and you want your app to have access to this.props.history.
 import { withRouter } from "react-router-dom";
 //Import connect from redux to have access your initialState in the reducer, and all it's actions.
-import { connect } from "react-redux";
+// import { connect } from "react-redux";
 //import actions that will be implemented in your reducer.
-import { login, logout } from "./redux/reducer";
+// import { login, logout } from "./redux/reducer";
 //import axios to get your user session from your backend.
 import axios from "axios";
+import { useAuth0 } from "./react-auth0-spa";
 import "./App.css";
 
-class App extends Component {
+// class App extends Component {
   //In it's componentDidMount  get the session, and if it has data set your intialState user to it.
-  componentDidMount() {
-    axios.get("/api/user-data").then(res => {
-      //Destruct the dispatch props from this.props that will be responsible for dispatching or initiating actions from your reducer.
-      const { dispatch } = this.props;
-      if (res.data.user) {
-        //Dispatch the login function with the user data.
-        dispatch(login(res.data.user));
-        //Else logout the user from the intialState.
-      } else {
-        //Dispatch the logout the user by default if there is no data in session.
-        dispatch(logout());
-      }
-    });
-  }
-  render() {
-    return (
-      <div className="App">
-        <Navbar />
-        {routes}
-      </div>
-    );
-  }
-}
+  // componentDidMount() {
+  //   axios.get("/api/user-data").then(res => {
+  //     //Destruct the dispatch props from this.props that will be responsible for dispatching or initiating actions from your reducer.
+  //     const { dispatch } = this.props;
+  //     if (res.data.user) {
+  //       //Dispatch the login function with the user data.
+  //       dispatch(login(res.data.user));
+  //       //Else logout the user from the intialState.
+  //     } else {
+  //       //Dispatch the logout the user by default if there is no data in session.
+  //       dispatch(logout());
+  //     }
+  //   });
+  // }
+  // render() {
+  //   return (
+  //     <div className="App">
+  //       <Navbar />
+  //       {routes}
+  //     </div>
+  //   );
+  // }
+// }
 
 //THen connect your app to your reducer.
-export default withRouter(connect()(App));
+// export default withRouter(connect()(App));
+
+function App() {
+  const { loading } = useAuth0();
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="App">
+      <header>
+        <NavBar />
+        {routes}
+      </header>
+    </div>
+  );
+}
+
+export default withRouter(App);
--- src/index.js
+++ src/index.js
@@ -10,12 +10,49 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 // import registerServiceWorker from './registerServiceWorker';
 
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </Provider>,
+//   document.getElementById("root")
+// );
+// registerServiceWorker();
+
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { Auth0Provider } from "./react-auth0-spa";
+import config from "./auth_config.json";
+import history from "./utils/history";
+
+// A function that routes the user to the right place
+// after login
+const onRedirectCallback = appState => {
+  history.push(
+    appState && appState.targetUrl
+      ? appState.targetUrl
+      : window.location.pathname
+  );
+};
+
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
+  <Auth0Provider
+    domain={config.domain}
+    client_id={config.clientId}
+    redirect_uri={window.location.origin}
+    onRedirectCallback={onRedirectCallback}
+  >
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </Auth0Provider>,
   document.getElementById("root")
 );
-// registerServiceWorker();
+
+serviceWorker.unregister();
